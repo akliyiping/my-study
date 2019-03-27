d5635936c9eedc8d4e4703125293355f06dcefdd
liyiping <li.li@thoughtworks.com>
2019-03-27 09:37:16 +0800

    commit

--- select.sql
+++ select.sql
@@ -1,8 +1,8 @@
 select et.name, et.price, p.real_price, (et.price-p.real_price)/p.real_price*100 as premium_rate   from
 
   (select epu.equipment_label, sum(epu.size*pup.price) as real_price from  equipment_property_unit as epu,
-                                               property_unit_price as pup
-  where  pup.unit_label = epu.unit_label
-  group by equipment_label) as p left join equipment et on p.equipment_label = et.label
-  where et.price > 1500
-  order by premium_rate asc
+                                                                           property_unit_price as pup
+   where  pup.unit_label = epu.unit_label
+   group by equipment_label) as p left join equipment et on p.equipment_label = et.label
+  #   where et.price > 1500
+order by premium_rate asc
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CommonModule } from './common/common.module';
 import { EquipmentModule } from './equipment/equipment.module';
-import {Connection} from 'typeorm';
-import { TypeOrmModule } from '@nestjs/typeorm';
-
-import {join} from "path";
 
 @Module({
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
   imports: [CommonModule, EquipmentModule],
 })
 export class AppModule {
